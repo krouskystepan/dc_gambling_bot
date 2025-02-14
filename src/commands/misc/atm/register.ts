e8df7a49f5b9c184cd7b835510eb30c36316e748
Stepan Krousky <stepan.krousky@seznam.cz>
2025-02-14 16:10:01 +0100

    Add ATM channel validation and logging to user registration command

--- src/commands/misc/atm/register.ts
+++ src/commands/misc/atm/register.ts
@@ -3,8 +3,14 @@ import {
   checkUserRegistration,
   formatNumberToReadableString,
 } from '../../../utils/utils'
-import { MessageFlags } from 'discord.js'
+import {
+  EmbedBuilder,
+  GuildMember,
+  MessageFlags,
+  TextChannel,
+} from 'discord.js'
 import User from '../../../models/User'
+import GuildConfiguration from '../../../models/GuildConfiguration'
 
 export const data: CommandData = {
   name: 'register',
@@ -15,7 +21,7 @@ export const options: CommandOptions = {
   deleted: false,
 }
 
-export async function run({ interaction }: SlashCommandProps) {
+export async function run({ interaction, client }: SlashCommandProps) {
   try {
     await interaction.deferReply({ flags: MessageFlags.Ephemeral })
 
@@ -24,6 +30,47 @@ export async function run({ interaction }: SlashCommandProps) {
     if (user) {
       return interaction.editReply('Už jsi zaregistrován.')
     }
+
+    const guildConfiguration = await GuildConfiguration.findOne({
+      guildId: interaction.guildId,
+    })
+
+    if (!guildConfiguration?.atmChannelIds.logs) {
+      return interaction.editReply('ATM ještě není nastaven. Počkejte prosím.')
+    }
+
+    if (!guildConfiguration?.atmChannelIds.actions) {
+      return interaction.editReply(`Tento příkaz zatím není nastaven.`)
+    }
+
+    if (guildConfiguration?.atmChannelIds.actions !== interaction.channelId) {
+      return interaction.editReply(
+        `Tento příkaz můžeš použít pouze v kanálu <#${guildConfiguration.atmChannelIds.actions}>`
+      )
+    }
+
+    const logChannel = client.channels.cache.get(
+      guildConfiguration.atmChannelIds.logs
+    ) as TextChannel
+
+    const member = interaction.member as GuildMember | null
+    const displayName =
+      member?.displayName ||
+      interaction.user.globalName ||
+      interaction.user.username
+
+    logChannel
+      .send({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle(
+              `Registrace uživatele ${displayName} (${interaction.user.username})`
+            )
+            .setColor('Purple')
+            .setDescription(`Uživatel se úspěšně zaregistroval.`),
+        ],
+      })
+      .catch(console.error)
 
     const newUser = new User({
       userId: interaction.user.id,
